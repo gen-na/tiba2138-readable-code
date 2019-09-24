7516a053a71d92518bbd7f9744cb4a5e96a6cd92
Takayuki Chiba <takayuki.chiba@justsystems.com>
2019-09-24 11:27:38 +0900

    spec1: 単語情報を出力できること

---
+++ src/jp/justsystems/readablecode/Dictionary.java
@@ -0,0 +1,12 @@
+package jp.justsystems.readablecode;
+
+/**
+ * 仕様1：仕様1：単語情報を出力できること
+ */
+public class Dictionary {
+
+    public static void main(String[] args) {
+        String word = "上手";
+        System.out.println(word);
+    }
+}
